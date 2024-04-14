2c69117aaafd27afc70fe47f1f4c7d6b927d950c
NoahDaniels1 <135069618+NoahDaniels1@users.noreply.github.com>
2024-04-14 16:19:24 -0500

    adds functionality to adding a restroom

--- client/src/Components/Add_Restroom/addRestroom.tsx
+++ client/src/Components/Add_Restroom/addRestroom.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { db } from "../../firebase.ts";
+import {collection, addDoc, serverTimestamp, } from 'firebase/firestore'
 import './addRestroom.css';
 
 function AddRestroom() {
@@ -15,7 +17,12 @@ function AddRestroom() {
     unisex: '',
     changingTable: '',
     directions: '',
-    comments: ''
+    comments: '',
+    created_at: '',
+    latitude: '',
+    longitude: '',
+    thumbs_up: 0,
+    thumbs_down: 0
   });
 
   const handleChange = (e) => {
@@ -25,10 +32,31 @@ function AddRestroom() {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formState);
-    // Add logic to handle form submission like sending data to a server or updating state
+    try {
+      const { street, city, state, country } = formState;
+      const address = `${street}, ${city}, ${state}, ${country}`;
+      const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyDLRmzWGSVuOYRHHFJ0vrEApxLuSVVgf1o`;
+      const response = await fetch(geocodeUrl);
+      const data = await response.json();
+
+      if (data.results.length > 0) {
+        const location = data.results[0].geometry.location;
+        const latitude = location.lat;
+        const longitude = location.lng;
+        const timestamp = serverTimestamp();
+        const formDataWithTimestamp = { ...formState, latitude, longitude, created_at: timestamp }
+        const docRef = await addDoc(collection(db, 'restrooms'), formDataWithTimestamp);
+        console.log("Document written with ID: ", docRef.id);
+        navigate('/dashboard');
+      } else {
+        console.error("Error geocoding address: Address not found");
+      }
+  } catch (error) {
+      console.error("Error adding document: ", error);
+  }
   };
 
   return (
@@ -64,7 +92,7 @@ function AddRestroom() {
       <textarea name="comments" placeholder="Have to be a paying customer..., etc." onChange={handleChange}></textarea>
       </label>
         <div className="form-actions">
-          <button type="submit" onClick={() => navigate('/dashboard')}>Save Restroom</button>
+          <button type="submit">Save Restroom</button>
           <button type="button" onClick={() => navigate('/')}>Cancel</button>
         </div>
       </form>
